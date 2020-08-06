0742a2c415669c5ce89d7f3ab95b9ae7eae5bad9
MDrFaza <mdrfaza21@gmail.com>
2020-08-06 20:48:56 +0700

    Add_Project_File

---
+++ File Project/Assets/Script/Bird.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class Bird : MonoBehaviour
+{
+    //Global Variables
+    [SerializeField] private float upForce = 100;
+    [SerializeField] private bool isDead;
+    [SerializeField] private UnityEvent OnJump, OnDead;
+    [SerializeField] private int score;
+    [SerializeField] private UnityEvent OnAddPoint;
+    private Rigidbody2D rigidBody2d;
+    private Animator animator;
+    [SerializeField] private Text scoreText;
+    public GameObject bulletPrefab;
+    
+
+    //init variable
+    void Start()
+    {
+        //Mendapatkan komponent ketika game baru berjalan
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        //Mendapatkan komponen animator pada game object   
+        animator = GetComponent<Animator>();
+    }
+    //Update setiap frame  
+    void Update()
+    {
+        //Melakukan pengecekan jika belum mati dan klik kiri pada mouse
+        if (!isDead && Input.GetMouseButtonDown(0))
+        {
+            //Burung meloncat
+            Jump();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            shootBullet();
+        }
+    }
+    //Fungsi untuk mengecek sudah mati apa belum
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    //Membuat Burung Mati
+    public void Dead()
+    {
+        //Pengecekan jika belum mati dan value OnDead tidak sama dengan Null
+        if (!isDead && OnDead != null)
+        {
+            //Memanggil semua event pada OnDead
+            OnDead.Invoke();
+        }
+
+        //Mengeset variable Dead menjadi True
+        isDead = true;
+
+
+    }
+
+    void Jump()
+    {
+        //Mengecek rigidbody null atau tidak
+        if (rigidBody2d)
+        {
+            //menghentikan kecepatan burung ketika jatuh
+            rigidBody2d.velocity = Vector2.zero;
+
+            //Menambahkan gaya ke arah sumbu y agar burung meloncat
+            rigidBody2d.AddForce(new Vector2(0, upForce));
+        }
+
+        //Pengecekan Null variable
+        if (OnJump != null)
+        {
+            //Menjalankan semua event OnJump event
+            OnJump.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //menghentikan Animasi Burung ketika bersentukan dengan object lain
+        animator.enabled = false;
+        
+        
+    }
+
+    public void AddScore(int value)
+    {
+        //Menambahkan Score value
+        score += value;
+
+        //Pengecekan Null Value
+        if (OnAddPoint != null)
+        {
+            //Memanggil semua event pada OnAddPoint
+            OnAddPoint.Invoke();
+        }
+
+        //Mengubah nilai text pada score text
+        scoreText.text = score.ToString();
+    }
+
+    public void shootBullet()
+    {
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = GetComponent<Rigidbody2D>().transform.position;
+    }
+}
---
+++ File Project/Assets/Script/Missile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 1;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "asteroid")
+        {
+            GameObject e = Instantiate(explosion) as GameObject;
+            e.transform.position = transform.position;
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
+
